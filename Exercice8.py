878eef0fb51f2ae82267bb4c1a21872b66eaf974
louis.hameau <louis.hameau@campus-centre.fr>
2025-06-27 12:30:30 +0200

    Commit message

--- Exercice8.py
+++ Exercice8.py
@@ -1,14 +1,15 @@
+from colorama import init, Fore
+
 def afficher_nom_encadre():
+    init(autoreset=True)  # Initialise colorama
     nom = input("Quel est votre prénom ? ")
     longueur = len(nom)
     ligne_haut = "┌" + "─" * (longueur + 2) + "┐"
-    ligne_milieu = f"│ {nom} │"
+    ligne_milieu = f"│ {Fore.RED}{nom}{Fore.RESET} │"
     ligne_bas = "└" + "─" * (longueur + 2) + "┘"
 
     print(ligne_haut)
     print(ligne_milieu)
     print(ligne_bas)
 
-
-# Appel de la fonction
 afficher_nom_encadre()
